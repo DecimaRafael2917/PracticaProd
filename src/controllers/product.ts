933b92234a221345ab72fea248fb6fbe96bb5450
rafael decima <rafade6@gmail.com>
2023-05-11 22:03:09 -0300

    tarea get id

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -7,6 +7,7 @@ const products : Product[] = [];
 export const createProduct = (req: Request, res: Response) => {
 
     // const id = req.params.id;
+    const { id } = req.params;
 
     // receive product from body
     const productNew : Product = req.body;
@@ -26,4 +27,19 @@ export const createProduct = (req: Request, res: Response) => {
     res.status(202).send({
         ...productNew
     })
+}
+export function getProductById(req:Request, res:Response){
+
+    const { id } = req.params;
+
+
+    const product = products.filter(p =>p.name.toString() === id);
+    if (product.length === 0) {
+        return res.status(404).send({
+            message: `product not exists ${id}`
+        }).json();
+    } 
+    return res.send({
+        ...product[0]
+    }).json();
 }
